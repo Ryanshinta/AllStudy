bbd688db95011a79bf30a507caffa501327cd171
Ryanshinta <ryanouyangyunru@gmail.com>
2022-05-30 16:55:59 +0800

    Initial commit

---
+++ src/main/java/live/allstudy/AllStudyApplication.java
@@ -0,0 +1,13 @@
+package live.allstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/allstudy/AllStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package live.allstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
