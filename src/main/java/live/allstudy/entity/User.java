63bdf869a4d015f9cab34219bfa037d0766c5136
Ryanshinta <ryanouyangyunru@gmail.com>
2022-09-05 09:22:38 +0000

    User entity add Createdtime and update time

--- src/main/java/live/allstudy/entity/User.java
+++ src/main/java/live/allstudy/entity/User.java
@@ -6,6 +6,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.lang.Nullable;
+
 import java.util.Date;
 
 @Table(name = "user")
@@ -31,9 +36,13 @@ public class User {
     @Column(columnDefinition = "default 1")
     private boolean status;
 
+    //give a defaule avatar later todo
     private String avatar;
 
+    @CreatedDate
+    @Column(name = "create_time", updatable = false)
     private Date create_time;
 
+    @UpdateTimestamp
     private Date update_time;
 }
--- src/test/java/live/allstudy/SpringDataTest.java
+++ src/test/java/live/allstudy/SpringDataTest.java
@@ -4,16 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import live.allstudy.entity.User;
 import live.allstudy.repository.UserRepository;
+import live.allstudy.service.UserDetailsService;
 
 @SpringBootTest
 public class SpringDataTest {
     
     @Autowired
-    private UserRepository userRepository;
+    private UserDetailsService userDetailsService;
 
     @Test
     public void testSpringData() {
-        
+        User user = new User();
+        user.setCreate_time(create_time);
+        userDetailsService.saveUser(user)
     }
 }
